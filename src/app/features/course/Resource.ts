0f9f2925aaad8ab6b8bc9acc9a819a7edfa9d3cd
kuzio <weronikka004@gmail.com>
2024-06-07 19:09:41 +0200

    createCourseComponent

---
+++ src/app/features/course/Resource.ts
@@ -0,0 +1,37 @@
+class Resource {
+  constructor(public _id: number, public _title: string, public _url: string, public _type: string) {
+
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  set title(value: string) {
+    this._title = value;
+  }
+
+  get url(): string {
+    return this._url;
+  }
+
+  set url(value: string) {
+    this._url = value;
+  }
+
+  get type(): string {
+    return this._type;
+  }
+
+  set type(value: string) {
+    this._type = value;
+  }
+  get id(): number {
+    return this._id;
+  }
+
+  set id(value: number) {
+    this._id = value;
+  }
+
+}
