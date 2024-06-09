169fca084cfacca4bab10849fa043d70cd6aa6d5
kuzio <weronikka004@gmail.com>
2024-06-09 15:46:09 +0200

    poprawki wizualne

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -5,6 +5,7 @@ import {MatInput} from "@angular/material/input";
 import {RegisterService} from "../../service/auth/register.service";
 import {Router} from "@angular/router";
 import {SignUpRequest} from "../../service/auth/signUpRequest";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-register',
@@ -13,7 +14,8 @@ import {SignUpRequest} from "../../service/auth/signUpRequest";
     MatFormField,
     FormsModule,
     MatInput,
-    MatLabel
+    MatLabel,
+    MatButton
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
